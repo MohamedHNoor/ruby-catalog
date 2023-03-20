7ae547aa5424bad64a4d2b8504480766f78add4d
Harshita Jain <harshitaforever06@gmail.com>
2023-03-20 22:46:02 +0530

    Add 1-to-many relationships in Item class

--- item.rb
+++ item.rb
@@ -1,12 +1,28 @@
 class Item
-    attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+    attr_accessor :id, :genre, :author, :label, :publish_date, :archived
 
   def initialize(id, :genre, author, source, label, publish_date, archived)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @source = source
-    @label = label
+    @genre = nil
+    @author = nil
+    @label = nil
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
+
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
 end
