f0ba04b7750d5d57225d946fa8bcb05bb9ec8080
rbhatt199924 <rbhatt199924@gmail.com>
2023-03-21 20:11:44 +0530

    update book

--- classes/book.rb
+++ classes/book.rb
@@ -1,10 +1,10 @@
-require 'item'
+require_relative 'item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state, :name
 
   def initialize(name, publisher, cover_state, publish_date)
-    super(name, publish_date)
+    super(publish_date)
     @name = name
     @publisher = publisher
     @cover_state = cover_state
