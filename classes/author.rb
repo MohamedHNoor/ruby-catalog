1c06e00a3458bda66e3fb99478599616839568f1
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-03-21 09:47:29 +0200

    Update add_author method inside author class and item class

--- classes/author.rb
+++ classes/author.rb
@@ -1,9 +1,9 @@
 require_relative './game'
 
 class Author
-  attr_accessor :first_name, :last_name, :id, :items
+  attr_accessor :id, :first_name, :last_name, :items
 
-  def initialize(first_name, last_name, id = nil)
+  def initialize(id, first_name, last_name)
     @id = id || rand(1..1000)
     @first_name = first_name
     @last_name = last_name
