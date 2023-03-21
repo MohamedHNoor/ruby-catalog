4bbb19a6ab31c0d333d82281f19a9f824c58f172
rbhatt199924 <rbhatt199924@gmail.com>
2023-03-21 11:51:17 +0530

    add book methods

--- classes/book.rb
+++ classes/book.rb
@@ -1,3 +1,5 @@
+require 'item'
+
 class Book < Item
   attr_accessor :publisher, :cover_state, :name
 
@@ -6,5 +8,10 @@ class Book < Item
     @name = name
     @publisher = publisher
     @cover_state = cover_state
+  end
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
   end
 end
