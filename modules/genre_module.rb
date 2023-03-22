91b6140f12216cd252e66e306b95ccb5e860c56f
Harshita Jain <harshitaforever06@gmail.com>
2023-03-22 17:47:26 +0530

    Add genre module

--- modules/genre_module.rb
+++ modules/genre_module.rb
@@ -1,34 +1,33 @@
-require_relative '../classes/label'
+require_relative '../classes/genre'
 require 'json'
 require 'date'
 
-module LabelsData
-  def load_labels
-    file = './json_db/labels.json'
+module GenreData
+  def load_genre
+    file = './json_db/genre.json'
     data = []
     if File.exist?(file) && !File.empty?(file)
       JSON.parse(File.read(file)).each do |element|
-        data.push(Label.new(element['id'].to_i, element['title'], element['color']))
+        data.push(Genre.new(element['id'].to_i, element['name']))
       end
     end
     data
   end
 
-  def save_labels
+  def save_genre
     data = []
-    @labels.each do |label|
-      data.push({ id: label.id, title: label.title, color: label.color })
+    @genres.each do |genre|
+      data.push({ id: genre.id, name: genre.name})
     end
-    File.write('./json_db/labels.json', JSON.pretty_generate(data))
+    File.write('./json_db/genre.json', JSON.pretty_generate(data))
   end
 
-  def create_label(string, item)
-    puts "Enter #{string}'s label title:"
-    title = gets.chomp
-    puts "Enter #{string}'s label color:"
-    color = gets.chomp
-    label = Label.new(@labels.length + 1, title, color)
-    label.add_item(item)
-    @labels << label
+  def create_genre(items)
+    puts "Enter genre name:"
+    name = gets.chomp
+    genre = Genre.new(@genres.length + 1, name)
+    genre.add_item(items)
+    @genres << genre
+    save_genre
   end
 end
